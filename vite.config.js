2bb6bbabc7f1b1e5b2b7ae3260c31e49da92fdca
Bloodyevil1 <adityagundear@gmail.com>
2025-08-12 15:00:32 +0530

    Updated styles, config, and package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Demo_P_Project/', // 👈 your repo name
+  base: '/Demo_P_Project/', // <-- Repo name
 })
